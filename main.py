fa0ad65c25c00b700252f88180958661d4b79787
mgsong83 <mgsong83@gmail.com>
2022-03-13 19:07:42 +0900

    Add some comment for code

--- main.py
+++ main.py
@@ -5,14 +5,18 @@ from selenium.webdriver.common.keys import Keys
 
 
 chrome_opt = Options()
+# to stay browser open when code ends (need to quit() or close() to close browser)
 chrome_opt.add_experimental_option("detach", True)
+# to remove bluetooth related error (don't know why)
 chrome_opt.add_experimental_option("excludeSwitches", ["enable-logging"])
 
-browser = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_opt)
+browser = webdriver.Chrome(ChromeDriverManager(
+).install(), options=chrome_opt)  # use chrome
 browser.get("https://google.com")
 
-search_bar = browser.find_element_by_class_name("gLFyf")
-search_bar.send_keys("rtx3070")
+search_bar = browser.find_element_by_class_name(
+    "gLFyf")  # search bar class name (get from inspect)
+search_bar.send_keys("rtx3070")  # key word
 search_bar.send_keys(Keys.ENTER)
 
 search_results = browser.find_elements_by_class_name("g")
