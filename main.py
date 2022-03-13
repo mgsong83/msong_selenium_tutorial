b13e12f15ca4c5ba14bfabd79808d8cfa82a80a9
mgsong83 <mgsong83@gmail.com>
2022-03-13 19:55:47 +0900

    refactoring (using Class)

--- main.py
+++ main.py
@@ -7,41 +7,55 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
-
 KEYWORD = "RTX 3080"
 
-chrome_opt = Options()
+
+class GoogleKeywordScreenShoter:
+
+    def __init__(self, Keyword, screen_dir):
+        chrome_opt = Options()
 # to stay browser open when code ends (need to quit() or close() to close browser)
-chrome_opt.add_experimental_option("detach", True)
+        chrome_opt.add_experimental_option("detach", True)
 # to remove bluetooth related error (don't know why)
-chrome_opt.add_experimental_option("excludeSwitches", ["enable-logging"])
+        chrome_opt.add_experimental_option(
+            "excludeSwitches", ["enable-logging"])
+        self.browser = webdriver.Chrome(
+            ChromeDriverManager().install(), options=chrome_opt)  # use chrome
+        self.keyword = Keyword
+        self.screendir = screen_dir
 
-browser = webdriver.Chrome(ChromeDriverManager(
-).install(), options=chrome_opt)  # use chrome
-browser.get("https://google.com")
+    def start(self):
+        self.browser.get("https://google.com")
 
-search_bar = browser.find_element_by_class_name(
-    "gLFyf")  # search bar class name (get from inspect)
-search_bar.send_keys(KEYWORD)  # key word
-search_bar.send_keys(Keys.ENTER)
+        search_bar = self.browser.find_element_by_class_name(
+            "gLFyf")  # search bar class name (get from inspect)
+        search_bar.send_keys(self.keyword)  # key word
+        search_bar.send_keys(Keys.ENTER)
 
-shitty_element = WebDriverWait(browser, 10).until(
-    EC.presence_of_element_located((By.CLASS_NAME, "qGXjvb")))  # wait until AD load
+        shitty_element = WebDriverWait(self.browser, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "qGXjvb")))  # wait until AD load
 
-# use JS code in python
-browser.execute_script(
-    """
-    const elements_to_remove = arguments[0];
-    elements_to_remove.parentElement.removeChild(elements_to_remove);
-    """,
-    shitty_element
-)  # remove AD contents
+        # use JS code in python
+        self.browser.execute_script(
+            """
+            const elements_to_remove = arguments[0];
+            elements_to_remove.parentElement.removeChild(elements_to_remove);
+            """,
+            shitty_element
+        )  # remove AD contents
+        search_results = self.browser.find_elements_by_class_name("g")
+        print(search_results)
+
+        for index, search_result in enumerate(search_results):
+            title = search_result.find_element_by_tag_name("h3")
+            if title:
+                print(title.text)
+            search_result.screenshot(
+                f"{self.screendir}/{self.keyword}_{index}.png")
+
+    def finish(self):
+        self.browser.quit()
 
 
-search_results = browser.find_elements_by_class_name("g")
-print(search_results)
-
-for search_result in search_results:
-    title = search_result.find_element_by_tag_name("h3")
-    if title:
-        print(title.text)
+rtx3070 = GoogleKeywordScreenShoter("RTX3070", "screenshot")
+rtx3070.start()
